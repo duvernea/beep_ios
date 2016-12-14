ade925f0d2020a844f18df8784025c1eaa2ad47d
BRIAN DUVERNEAY <duverneay@gmail.com>
2016-12-13 19:48:41 -0800

    feat: Add BoardsViewController (UITableViewController)

---
+++ beep/BoardsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  BoardsViewController.swift
+//  beep
+//
+//  Created by BRIAN DUVERNEAY on 12/13/16.
+//  Copyright © 2016 Peast. All rights reserved.
+//
+
+import UIKit
+
+class BoardsViewController: UITableViewController {
+    
+}
