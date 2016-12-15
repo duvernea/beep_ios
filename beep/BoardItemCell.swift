5e86755fd532575afbeb8d82158b77ae910e0bda
BRIAN DUVERNEAY <duverneay@gmail.com>
2016-12-15 11:25:32 -0800

    feat: Add BoardItemCell for use in TableView

---
+++ beep/BoardItemCell.swift
@@ -0,0 +1,15 @@
+//
+//  BoardItemCell.swift
+//  beep
+//
+//  Created by BRIAN DUVERNEAY on 12/15/16.
+//  Copyright © 2016 Peast. All rights reserved.
+//
+
+import UIKit
+
+class BoardItemCell: UITableViewCell {
+    
+    @IBOutlet var boardNameLabel: UILabel!
+    @IBOutlet var boardImageView: UIImageView!
+}
