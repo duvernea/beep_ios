455ef6f32125234c8b7027415d0c233d97b98623
BRIAN DUVERNEAY <duverneay@gmail.com>
2016-12-15 13:32:39 -0800

    feat: Add new detail Boards Controller and segue to it

---
+++ BoardsDetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  BoardsDetailViewController.swift
+//  beep
+//
+//  Created by BRIAN DUVERNEAY on 12/15/16.
+//  Copyright © 2016 Peast. All rights reserved.
+//
+
+import UIKit
+
+class BoardsDetailViewController: UIViewController {
+    @IBOutlet var textLabel: UILabel!
+    
+    var board: Board!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //textLabel.text = board.name
+        textLabel.text = "TEST TEST TEST"
+    }
+}
